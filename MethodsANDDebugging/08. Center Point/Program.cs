f4fb60b0948c1acc6d13d0b9a932397d5833fdc4
Tony <todiros@gmail.com>
2017-06-06 22:02:04 +0300

    Updated MethodsANDDebugging.sln

---
+++ MethodsANDDebugging/08. Center Point/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Center_Point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x1 = double.Parse(Console.ReadLine());
+            var y1 = double.Parse(Console.ReadLine());
+            var x2 = double.Parse(Console.ReadLine());
+            var y2 = double.Parse(Console.ReadLine());
+            PrintPointClosestToCenter(x1, y1, x2, y2);
+
+        }
+
+        static void PrintPointClosestToCenter(double x1, double y1, double x2, double y2)
+        {
+            decimal diagonalX1Y1 = (decimal)Math.Sqrt(Math.Pow(x1, 2) + Math.Pow(y1, 2));
+            decimal diagonalX2Y2 = (decimal)Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+            if (diagonalX1Y1 < diagonalX2Y2)
+            {
+                Console.WriteLine("({0}, {1})", x1, y1);
+            }
+            else if (diagonalX1Y1 > diagonalX2Y2)
+            {
+                Console.WriteLine("({0}, {1})", x2, y2);
+            }
+            else
+            {
+                Console.WriteLine("({0}, {1})", x1, y1);
+            }
+        }
+    }
+}
---
+++ MethodsANDDebugging/MethodsANDDebugging/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Prime_Checker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = long.Parse(Console.ReadLine());
+            bool isitPrime = IsPrime(input);
+            Console.WriteLine(isitPrime);
+        }
+
+        private static bool IsPrime(long input)
+        {
+
+            var boundary = (int)Math.Floor(Math.Sqrt(input));
+            if (input == 0 || input == 1) return false;   
+            if (input == 2) return true;
+
+            for (int i = 2; i <= boundary; i++)
+            {
+                if (input % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;    
+        }
+    }
+}
